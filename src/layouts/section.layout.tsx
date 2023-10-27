763272cc34c8b5e518c076ba0deff957f9caaa9d
Prajna <prajnapprabhu3@gmail.com>
2023-10-27 16:40:40 +0530

    ref: folder structure

---
+++ src/layouts/section.layout.tsx
@@ -0,0 +1,7 @@
+// interface SectionProps {
+//   children: React.ReactNode;
+// }
+
+export default function Section({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
